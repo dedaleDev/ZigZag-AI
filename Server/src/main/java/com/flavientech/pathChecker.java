6aafda83c97b7563aae71a7a1f2b27e836b3d2f1
Diéval Flavien <dievalflavien@protonmail.com>
2025-01-04 19:50:05 +0100

    fixed minor bug in pathChecker for better compatibility with eclipse and other running config

--- Server/src/main/java/com/flavientech/pathChecker.java
+++ Server/src/main/java/com/flavientech/pathChecker.java
@@ -3,17 +3,16 @@ package com.flavientech;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.File;
 import java.io.IOException;
 
 public class pathChecker {
     public static String checkPath(String fileName) {
         Path currentDir = Paths.get(System.getProperty("user.dir"));
-        Path serverDir = currentDir.resolve("Server");
-
         try {
-            return Files.walk(serverDir)
+            return Files.walk(currentDir)
                         .filter(Files::isRegularFile)
-                        .filter(p -> !p.toString().contains("target")) // Exclude 'target' directory
+                        .filter(p -> !p.toString().contains("target"))
                         .filter(p -> p.getFileName().toString().equals(fileName))
                         .map(Path::toAbsolutePath)
                         .map(Path::toString)
@@ -25,12 +24,19 @@ public class pathChecker {
         }
     }
 
-    public static String getCachesDir(){
+    public static String getCachesDir() {
         Path currentDir = Paths.get(System.getProperty("user.dir"));
-        if (currentDir.endsWith("ZigZag")) {
-            return currentDir.toString() + "/Server/src/caches/";
+        Path cachesDir = currentDir.resolve("Server/src/caches");
+        if (Files.exists(cachesDir)) {
+            return cachesDir.toAbsolutePath().toString() + File.separator;
+        } else {
+            cachesDir = currentDir.resolve("src/caches");
+            if (Files.exists(cachesDir)) {
+                return cachesDir.toAbsolutePath().toString() + File.separator;
+            } else {
+                return "Caches directory not found";
+            }
         }
-        return currentDir + "../flavientech/com/java/caches/";
     }
     
     //public static void main(String[] args) {
