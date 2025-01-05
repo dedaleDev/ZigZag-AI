99be15870f17b6cfe41b8ecbd07c0412760959ed
Diéval Flavien <dievalflavien@protonmail.com>
2025-01-05 13:59:05 +0100

    Suppression de la méthode findByUserId dans FlashMemoryRepository

--- Server/src/main/java/com/flavientech/repository/FlashMemoryRepository.java
+++ Server/src/main/java/com/flavientech/repository/FlashMemoryRepository.java
@@ -3,8 +3,5 @@ package com.flavientech.repository;
 import com.flavientech.entity.FlashMemory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FlashMemoryRepository extends JpaRepository<FlashMemory, Long> {
-    List<FlashMemory> findByUserId(Long userId);
 }
